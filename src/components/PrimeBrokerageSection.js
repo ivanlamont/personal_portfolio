b6bc5d190ac195bd28727d0d8168c3c2131470f4
ivanlamont <ivanlamont@yahoo.com>
2024-01-10 21:19:23 -0800

    adding content to the financial page

--- src/components/PrimeBrokerageSection.js
+++ src/components/PrimeBrokerageSection.js
@@ -18,7 +18,18 @@ const PrimeBrokerageSection = (props) => (
         gap={2}
       >
         <GridItem colSpan={1} >
-          <Text fontSize={really_big_font}>Prime Brokerage</Text>
+          <Text fontSize="sm">
+          Prime Brokerage
+The primary concern in any Prime Brokerage, and the thing that sets this line of business apart from all its peers in Financial Services, is that positions are held overnight.
+This brings many additional responsibilities to bear.  First and foremost, positions must all be priced and described.  Pricing highly liquid securities such as NYSE listed equities is a science, but as we get away from liquidity, we find pricing becomes more of an art.  Selecting a reliable vendor with a readily explainable pricing algorithm is crucial.  
+Accounting Systems
+A very important early decision that any business must make when starting a Prime Brokerage business is the selection of the accounting platform that will be used to track positions, and produce reports.  There are many good choices available, including Experian, Charles River, and Geneva.  With a good platform, the business will be able to produce eye-catching reports that allow clients additional insight into their investments, and grow the AUM numbers.
+Margin, Risk, and Calls
+Pricing an investment means updating its value.  And that means that any associated leverage needs to be recalculated, and if necessary, calls may need to be made, if the Fed or House calls thresholds are breached.  During periods of high volatility, the business may need to recalculate these numbers in real-time ahead of market closure, in order to ensure that any calls that do occur overnight are of a manageable magnitude, before things get too far out of hand.
+Options – Exercises and Assignments
+Many traders these days like to include derivatives in their portfolios in order to enhance returns.  These bring with them their own challenges, especially with regard to short options positions that have American exercise style, as they may be assigned at any time by the holder of the long side of the option, with the notice being delivered by the CBOE to the custodian overnight.  These must all be processed correctly in time for the open of the following trading session.
+
+          </Text>
         </GridItem>        
       </Grid>  
     </FullScreenSection>
--- src/components/TradingSection.js
+++ src/components/TradingSection.js
@@ -20,7 +20,12 @@ const TradingSection = (props) => (
         gap={2}
       >
         <GridItem colSpan={1} >
-          <Text fontSize={really_big_font}>TradingSection</Text>
+          <Text fontSize='md'>The first trade management system I helped build, was for BancBoston Robertson Stevens.  We used C++ and built a three-tier architecture which was fault tolerant and used a proprietary ticketing system developed in-house.  The UI was touchscreens, installed on every trading desk.  By today's standards, it was pretty primitive.  It supported only equities, and had very limited order routing capability.  But, it worked.
+I took my skills to Conifer Securities where I led the effort to build a bigger, more capable system that managed not only trade execution, but order management, portfolio allocation, commissions, and risk.  Again built using the Microsoft stack, MS-SQL , C++ and VB, and later C#.  This was the first time I encountered the FIX protocol, used for sending trading messages over the wire.  Although the final product was successful, there were issues with latency that were very challenging to overcome.  Order routing was very basic, I could see there was scope for a great deal of innovation in this area, given enough time.
+Modern trading technology relies greatly on low latency connectivity to the market, in order to ensure systems have the most accurate data on the current market order book, and hit the best prices for every trade.  For this reason, C++ has often been chosen as the language of choice, indeed some claim it is the only choice, for trade execution code to be written in.
+However, I feel that this is no longer the case.  The additional overheads involved in using C++ have often been worth the effort required - but since the release of the .NET Core platform, there's now a serious challenger to this crown in the form of C#.
+The platform reaches it's tenth anniversary of launch this year (2024) and has reached a level of maturity, in particular with regard to improvements in the garbage collection of unused memory, that mean that it should be a serious candidate for new development in trade execution platforms.
+</Text>
         </GridItem>        
       </Grid>
     </FullScreenSection>
