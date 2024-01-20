1c0ce4803460e5ed22114a9fd153a7322c6cca14
ivanlamont <ivanlamont@yahoo.com>
2024-01-20 12:01:00 -0800

    mobile content cleanup

--- src/components/MobileSections.tsx
+++ src/components/MobileSections.tsx
@@ -14,28 +14,31 @@ const heading_size = "md"
 const Intro: React.FC = () => (
     <GridItem colSpan={2}>
         <Heading as={heading_type} size={heading_size}>Recent Mobile Development</Heading>
-        <Text>Mobile development wasn't even in it's infancy when Nathan approached me, with the idea to connect GPS signals to an MP3 player.</Text>
+        <Text>There aren't many coders out there that have been creating applications for handheld devices as long as I have!  In fact, Mobile development wasn't even in it's infancy when Nathan approached me, with the idea to connect GPS signals to an MP3 player.</Text>
     </GridItem>
 );
 
 const Historical: React.FC = () => (
     <GridItem>
-        <Text>Explaining a little about the history of mobile development, and how it has changed over the years.</Text>
+        <Text>I recently sat down with some STEM pupils at my sons school to talk about the history of the now-ubiquitous devices.  Here's an excerpt or two. </Text>
         <YouTube videoId={yt1_dev_since_2002} />
+        <Text>Explaining a little about the history of mobile development, and how it has changed over the years.</Text>
     </GridItem>
 );
 
 const Tooling: React.FC = () => (
     <GridItem>
-        <Text>Is it easier to write mobile code today, compared to two decades ago?</Text>
-        <YouTube videoId={yt2_tools_history} />
+        <Text>The leaps forward in mobile development are still coming.  </Text>
+        <YouTube videoId={yt3_null_safety} />
+        <Text>Proper NULL safety is a key factor in making applications zip along.  That's one of the things I really love about Flutter, as a development system.  I have used React Native also - it's good, and it's really nice that it shares so many structures with ReactJS.  The number of developers who can make the move from the web to the mobile world is much greater as a result.</Text>
     </GridItem>
 );
 
 const Languages: React.FC = () => (
     <GridItem>
-        <Text>All languages are the same, surely?</Text>
-        <YouTube videoId={yt3_null_safety} />
+        <Text>There are quite a few different languages and platforms that have been built, to allow us to create applications for mobile devices.  When I first started, they were just "PDAs" - short for Personal Digital Assistants.  This was a small palm-sized computer that you could use to track your appointments and take notes.  Initially, most of them did not have any networking abilities at all, certainly not telephony.  Wifi slowly came along, then cellular.  There was a period there around 2004 where carrying two or more devices was not uncommon.  It was obvious that a convergence was going to happen, and I was in the right place at the time to take advantage.</Text>
+        <YouTube videoId={yt2_tools_history} />
+        <Text>Tooling, however, was dreadful.  And there were many different platforms to target.  Which meant that you either had a small target installed base, or you had to find some sort of cross-platform technology.  Today Windows CE, PalmOS and BlackBerry are all gone.  We just have Android and Apple.</Text>
     </GridItem>
 );
 
@@ -48,6 +51,7 @@ const MobileHistorySectionBox: React.FC<ColorPageProps> = (props) => {
         <StandardGrid>
             <Intro />
             <Historical />
+            <Languages />
         </StandardGrid>
     </FullScreenSection>;  
 };
@@ -60,7 +64,6 @@ const MobileRecentSectionBox: React.FC<ColorPageProps> = (props) => {
     >  
         <StandardGrid>
             <Tooling />
-            <Languages />
         </StandardGrid>  
     </FullScreenSection>
 }
