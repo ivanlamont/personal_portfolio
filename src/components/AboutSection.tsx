3d3c7e8ba927a2d10811bc0ee2211748acf77a2f
ivanlamont <ivanlamont@yahoo.com>
2024-01-20 11:47:38 -0800

    about-content added

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -10,64 +10,91 @@ const yt_landing = "napcu2KH5EU";
 const heading_type = "h3"
 const heading_size = "md"
 
+type FlinkProps = {
+  url: string;
+  text: string;
+}
+
+const Flink: React.FC<FlinkProps> = (props) => {
+  return <Link href={props.url} target="_blank" color='teal.500'>{props.text}</Link>  
+}
+
 const Intro: React.FC = () => (
     <Box>
-        <Avatar
-              size='2xl'
-              name='Ivan J. Lamont'
-              src={require("../images/self_small.jpg")}
-            />
+      {/* <Avatar size='2xl' name='Ivan J. Lamont' src={require("../images/self_small.jpg")} /> */}
       <Heading as={heading_type} size={heading_size}>Extracurricular Activities</Heading>
       <Text></Text>
     </Box>
   );
 
 const Pilot: React.FC = () => (
-<Box>
+<GridItem>
     <Heading as={heading_type} size={heading_size}>Flying</Heading>
-    <Image src={require("../images/aviation.jpg")} alt="Flying" />
-    <Text>As a young lad growing up in Ireland, I always wanted to become a pilot.  But <Link href="https://aerlingus.com" target="_blank">Aer Lingus</Link> were never hiring, so I decided to follow my passion for computer programming instead.  When I moved to America, I spent some of my spare time learning to fly out of <Link href="https://www.oaklandairport.com/" target="_blank">Oakland</Link>.  So far I have earned my FAA Commercial Pilot license with an Instrument Rating.  I plan on getting my Certified Flight Instructor cert before the decade is out</Text>
-</Box>
+    <Text>As a young lad growing up in Ireland, I always wanted to become a pilot.</Text>
+    <Image margin={"9px"} height="200px" src={require("../images/aviation.jpg")} alt="Flying" />
+    <Text>Unfortunately, <Flink url="https://www.aerlingus.com" text="Aer Lingus" /> were never hiring, so I decided to follow my passion for computer programming instead.  When I moved to America, I spent some of my spare time learning to fly out of <Flink url="https://www.oaklandairport.com/" text="Oakland" />.  So far I have earned my FAA Commercial Pilot license with an Instrument Rating.  I plan on getting my Certified Flight Instructor cert before the decade is out</Text>
+</GridItem>
 );
 
+const FlightReview: React.FC = () => (
+  <GridItem>
+      <Text>Every two years we have to do a Biannual Flight Review with an instructor</Text>
+      <YouTube videoId={yt_flying} />
+      <Text>I decided to have a little fun, on this one - back to where it all began, into Oakland International, the north complex.  The runways have been renumbered as 28L and 28R now.  I first flew solo from 27R, I feel like they took it away from me!</Text>
+  </GridItem>
+  );
+
+const Landing: React.FC = () => (
+  <GridItem>
+      <Text>Landing a C-172 at Petaluma</Text>
+      <YouTube videoId={yt_landing} />
+      <Text>My son did a pretty good job with the camera, I think</Text>
+  </GridItem>
+  );
+  
 const GuitarBuilder: React.FC = () => (
-  <Box>
+  <GridItem>
       <Heading as={heading_type} size={heading_size}>Guitar Building</Heading>
       <Text>Rock & Roll has to come from somewhere, right?  I've made a few electric guitars, and I am in the process of making a few more.  This video shows the neck being shaped for one of the three replicas that I am making of the 1976 Gibson Explorer.  The wood is mahogany.  I plan to give one of each of these to my sons, when the time is right.</Text>
-  </Box>
+  </GridItem>
 );
 
 const RingZero: React.FC = () => (
-  <Box>
+  <GridItem>
       <Heading as={heading_type} size={heading_size}>Rock & Roll</Heading>
-      <Text>For a number of years, I was the lead singer and lead guitarist for Ring Zero.  It's hard to beat the thrill of playing guitar at full volume for a crowd - the adrenaline is incredible.  We played a few memorable shows, and I sometimes dream about "getting the band back together"!</Text>
+      <Text>For a number of years, I was the lead singer and lead guitarist for Ring Zero.  </Text>
       <Image src={require("../images/ring-zero.png")} alt="Referee" />  
-  </Box>
+      <Text>It's hard to beat the thrill of playing guitar at full volume for a crowd - the adrenaline is incredible.  We played a few memorable shows, and I sometimes dream about "getting the band back together"!</Text>
+  </GridItem>
 );
 
 const ManchesterUnited: React.FC = () => (
-  <Box>
+  <GridItem>
       <Heading as={heading_type} size={heading_size}>Glory, Glory...</Heading>
-      <Text>Mecca for any Manchester United fan is Old Trafford.  I've been there several times, and one day I hope to bring my three sons too.  Although one of them is a Chelsea fan!  Eeeew.</Text>
-      <Image src={require("../images/old-trafford.avif")} alt="Referee" />  
-  </Box>
+      <Text>Mecca for any <Flink url="https://www.manchesterunited.com/" text="Manchester United" /> fan is Old Trafford.  I've been there several times, and one day I hope to bring my three sons too.  Although one of them is, sadly, a Chelsea fan! </Text>
+      <Center><Image src={require("../images/old-trafford.avif")} alt="Referee" />  </Center>
+      <Text>Like most United fans, I remember so fondly the days of Sir Alex Ferguson - we hope some day to see a manager half as good at the helm!</Text>
+  </GridItem>
 );
 
 
 const ChessPlayer: React.FC = () => (
-  <Box>
+  <GridItem>
       <Heading as={heading_type} size={heading_size}>Chess</Heading>
-      <Text>I love Chess, I play a little every day.  I've had a rating of 1900 before, but it takes a lot of practice.  If you'd like to play me, the link to my chess.com account is in the header.</Text>
-      <Image src={require("../images/chess.jpg")} alt="Referee" />
-  </Box>
+      <Text>I love Chess, I play a little every day.  I've had a rating of 1900 before, but it takes a heck of a lot of practice to remain at the top of your game, and right now I'm pretty busy!</Text>
+      <Center>
+        <Image height={"420px"} src={require("../images/chess.jpg")} alt="Referee" alignContent={"center"} />
+      </Center>
+      <Text>I'm a d4 player.  None of this kings-pawn complications for me.  A nice open game please.  I know the <Flink url="https://www.amazon.com/s?k=Meran-Anti-Meran-Variations-Alexey-Dreev" text="Meran and Anti-Meran variations" /> and the Moscow quite well, be warned!  If you'd like to play me, the link to my chess.com account is in the header.</Text>
+  </GridItem>
   );
       
 const Referee: React.FC = () => (
-  <Box>
+  <GridItem>
       <Heading as={heading_type} size={heading_size}>Refereeing</Heading>
-      <Text>I've been a <Link href="https://www.ussoccer.com/referee-program" target="_blank">licensed soccer referee</Link> since 2019.  It's an excellent way to keep fit, and if you have the required temperament, it's a great experience.  I love it - if you love football, give it a shot!</Text>
+      <Text>I've been a <Flink url="https://www.ussoccer.com/referee-program" text="licensed soccer referee" /> since 2019.  It's an excellent way to keep fit, and if you have the required temperament, it's a great experience.  I love it - if you love football, give it a shot!</Text>
       <Image src={require("../images/referee.jpg")} alt="Referee" />  
-  </Box>
+  </GridItem>
 );
         
 
@@ -84,12 +111,10 @@ const AboutFlying: React.FC<ColorPageProps> = (props) => {
         <Pilot />
       </GridItem>
       <GridItem>
-        <Text>Here's a recent flight back to OAK</Text>
-        <YouTube videoId={yt_flying} />
+        <Landing />
       </GridItem>
       <GridItem>
-        <Text>Landing a C-172 at Petaluma</Text>
-        <YouTube videoId={yt_landing} />
+        <FlightReview />
       </GridItem>
     </StandardGrid>
   </FullScreenSection>;
